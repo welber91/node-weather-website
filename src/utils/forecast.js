f1b6570c0132882f5798a6292bad61613c55f479
Welber Wendler <wreis@mcontigo.com>
2019-04-09 22:15:00 -0300

    Add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' A temperatura atual é de ' + body.currently.temperature + ' C. Tem com probabilidade de ' + body.currently.precipProbability*100 + '% de chuva.')
+            callback(undefined, body.daily.data[0].summary + ' A temperatura atual é de ' + body.currently.temperature + ' C. Probabilidade de ' + body.currently.precipProbability*100 + '% de chuva.')
         }
     })
 }
